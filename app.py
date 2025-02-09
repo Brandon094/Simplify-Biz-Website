9a8c09a73e66155166b7134518151a31f54a285b
BrandCode_Solutions <87330304+Brandon094@users.noreply.github.com>
2025-02-09 03:11:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
 
 mail = Mail(app)
 
-@app.route('/index', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
